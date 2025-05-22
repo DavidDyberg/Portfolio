ee5cbb161ff83b95902375db3ae85617d2f6e014
David Dyberg <david.dyberg@gmail.com>
2025-05-22 11:42:45 +0200

    cleanup code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,8 +6,8 @@ export const Route = createFileRoute('/')({
 
 function App() {
   return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]"></header>
+    <div className="text-center h-screen bg-[#282c34]">
+      <p className="text-white text-2xl">Home</p>
     </div>
   )
 }
