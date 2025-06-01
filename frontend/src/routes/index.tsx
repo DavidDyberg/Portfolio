753098145afad42ea86cc7b92dee8c041d129c06
David Dyberg <david.dyberg@gmail.com>
2025-06-01 19:00:02 +0200

    feat: style navbar & global styling

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -6,7 +6,7 @@ export const Route = createFileRoute('/')({
 
 function App() {
   return (
-    <div className="text-center h-screen bg-[#282c34]">
+    <div>
       <p className="text-white text-2xl">Home</p>
     </div>
   )
