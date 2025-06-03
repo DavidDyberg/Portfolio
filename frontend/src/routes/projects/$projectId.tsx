479775feffc5bf43096c7810cfaf788a873647c1
David Dyberg <david.dyberg@gmail.com>
2025-06-03 11:53:10 +0200

    feat: prefetch data

--- frontend/src/routes/projects/$projectId.tsx
+++ frontend/src/routes/projects/$projectId.tsx
@@ -1,9 +1,26 @@
+import { fetchProject } from '@/api-routes/projects'
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/projects/$projectId')({
   component: RouteComponent,
+  pendingComponent: () => <div className="text-white">Loading...</div>,
+  errorComponent: () => <div>Error loading data</div>,
+  loader: async ({ context: { queryClient }, params }) => {
+    await queryClient.prefetchQuery({
+      queryKey: ['project', params.projectId],
+      queryFn: () => fetchProject(params.projectId),
+    })
+  },
 })
 
 function RouteComponent() {
-  return <div>Hello "/projects/$projectId"!</div>
+  const { projectId } = Route.useParams()
+
+  const { data } = useSuspenseQuery({
+    queryKey: ['project', projectId],
+    queryFn: () => fetchProject(projectId),
+  })
+
+  return <div className="text-white">{data.title}</div>
 }
