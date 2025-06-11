75535568e806eb3cd5e044e3156a65069724b092
David Dyberg <david.dyberg@gmail.com>
2025-06-11 11:56:53 +0200

    feat: add logout button if user is logged in

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { Link } from '@tanstack/react-router'
+import { useAuth } from '@/context/AuthContext'
 
 export default function Header() {
+  const { user, logout } = useAuth()
   return (
     <header className="p-2 flex gap-2 text-white justify-end">
       <nav className="flex flex-row">
@@ -11,9 +13,17 @@ export default function Header() {
         <div className="px-2 font-bold">
           <Link to="/projects">Projects</Link>
         </div>
-        <div className="px-2 font-bold">
-          <Link to="/login">Login</Link>
-        </div>
+        {user ? (
+          <div className="px-2 font-bold">
+            <button className="cursor-pointer" onClick={logout}>
+              LogOut
+            </button>
+          </div>
+        ) : (
+          <div className="px-2 font-bold">
+            <Link to="/login">Login</Link>
+          </div>
+        )}
       </nav>
     </header>
   )
