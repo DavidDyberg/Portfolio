82170cdb8cc513827f721a9911105089ba5dc407
David Dyberg <david.dyberg@gmail.com>
2025-06-11 12:03:20 +0200

    refactor: move styling to the perent element

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,22 +5,22 @@ export default function Header() {
   const { user, logout } = useAuth()
   return (
     <header className="p-2 flex gap-2 text-white justify-end">
-      <nav className="flex flex-row">
-        <div className="px-2 font-bold">
+      <nav className="flex flex-row font-bold">
+        <div className="px-2">
           <Link to="/">Home</Link>
         </div>
 
-        <div className="px-2 font-bold">
+        <div className="px-2">
           <Link to="/projects">Projects</Link>
         </div>
         {user ? (
-          <div className="px-2 font-bold">
+          <div className="px-2">
             <button className="cursor-pointer" onClick={logout}>
               LogOut
             </button>
           </div>
         ) : (
-          <div className="px-2 font-bold">
+          <div className="px-2">
             <Link to="/login">Login</Link>
           </div>
         )}
