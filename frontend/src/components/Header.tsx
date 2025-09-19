8173f845685fabd5e8cea6d3ed0d073220e04871
David Dyberg <david.dyberg@gmail.com>
2025-09-19 11:26:38 +0200

    feat: add activeProps to header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,11 +7,25 @@ export default function Header() {
     <header className="p-2 flex gap-2 text-white justify-end">
       <nav className="flex flex-row font-bold">
         <div className="px-2">
-          <Link to="/">Home</Link>
+          <Link
+            to="/"
+            activeProps={{
+              className: 'underline underline-offset-4',
+            }}
+          >
+            About
+          </Link>
         </div>
 
         <div className="px-2">
-          <Link to="/projects">Projects</Link>
+          <Link
+            to="/projects"
+            activeProps={{
+              className: 'underline underline-offset-4',
+            }}
+          >
+            Projects
+          </Link>
         </div>
         {user && (
           <div className="px-2">
